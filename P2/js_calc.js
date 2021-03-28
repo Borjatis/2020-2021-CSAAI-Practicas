debc6a5d62f19d41136aeedd740ec5f6322c92fc
Borjatis <borja.ca@gmail.com>
2021-03-28 21:14:27 +0200

    JS de calculadora, con retoques en el html

---
+++ P2/js_calc.js
@@ -0,0 +1,114 @@
+console.log("Ejecutando JS...");
+
+display = document.getElementById("display")
+delet = document.getElementById("delet")
+reset = document.getElementById("reset")
+coma = document.getElementById("coma")
+sqrt = document.getElementById('sqrt')
+igual = document.getElementById("igual")
+
+//Array de numeros
+let digitos = document.getElementsByClassName("num");
+//Array de operadores
+let operacion = document.getElementsByClassName("operacion");
+
+//-- Estados de la calculadora
+const ESTADO = {
+    INIT: 0,
+    OP1: 1,
+    OPERATION: 2,
+    OP2: 3
+}
+ 
+//-- Variable de estado de la calculadora
+//-- Al comenzar estamos en el estado incial
+let estado = ESTADO.INIT;   
+
+//--Bucle que lee cada digito que se pulsa, del 0 al 9
+for(i=0; i<digitos.length; i++){
+    digitos[i].onclick=(ev)=>{
+        digito(ev.target.value);
+    }
+  }
+  
+//--Bucle que lee cada operador que se pulsa, son
+//suma, resta, multiplicación, división, exponencial
+for (i=0; i<operacion.length; i++){
+    operacion[i].onclick = (ev)=> {
+        if(estado == ESTADO.OP1){
+            operaciones(ev.target.value);
+            console.log(`ESTADO ${estado}`);
+        }
+    }
+  }
+
+//-- Poner coma
+coma.onclick = () => {
+  if(estado == ESTADO.OP1 || estado == ESTADO.INIT || estado == ESTADO.OP2){
+    display.innerHTML += coma.innerHTML;
+  }
+}
+
+//-- Resolver raiz cuadrada
+sqrt.onclick = () => {
+    display.innerHTML = Math.sqrt(display.innerHTML);
+  }
+
+//-- Resolver operaciones
+igual.onclick = () => {
+    display.innerHTML = eval(display.innerHTML);
+    estado = ESTADO.OP1;
+    console.log(`ESTADO ${estado}`);
+}
+
+//-- Borrar el ultimo digito o operador
+delet.onclick = () => {
+    if((display.innerHTML == '0')||(display.innerHTML == '')){
+      display.innerHTML = '0';
+    }else {
+      display.innerHTML = display.innerHTML.slice(0,-1);
+    }
+  }
+
+//-- Poner a cero la expresion
+//-- Y volver al estado inicial
+reset.onclick = () => {
+    display.innerHTML = "0";
+    estado = ESTADO.INIT;
+    console.log(`ESTADO ${estado}`);
+}
+
+//-- Función de retrollamada de los digitos (estados)
+function digito(botones)
+{
+    //-- Se ha recibido un dígito
+    //-- Según en qué estado se encuentre la calculadora
+    //-- se hará una cosa u otra
+
+    //--Mostramos directamente digito en el display
+    if (estado == ESTADO.INIT) {
+        display.innerHTML = botones;
+        //-- Pasar al siguiente estado
+        estado = ESTADO.OP1;
+        console.log(`ESTADO ${estado}`);
+    } else if (estado==ESTADO.OP1) {       
+        //--En cualquier otro estado lo añadimos
+        display.innerHTML += botones;
+    } else if (estado==ESTADO.OPERATION) {       
+        display.innerHTML += botones;
+        //--Cuando hay operador añadimos y pasamos
+        estado = ESTADO.OP2;
+        console.log(`ESTADO ${estado}`);
+    } else if (estado==ESTADO.OP2) {       
+        display.innerHTML += botones;
+        //console.log(`ESTADO ${estado}`);
+    }    
+}
+
+//--Función de retrollamada de los operandos (estados)
+function operaciones(operacion){
+    if (estado != ESTADO.OPERATION) {
+      display.innerHTML += operacion;
+      estado = ESTADO.OPERATION;
+    }
+}
